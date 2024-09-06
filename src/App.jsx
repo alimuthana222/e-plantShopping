313c33b18c6f03afc85a86c0e7ce749ea176e2c1
alimuthana222 <153876752+alimuthana222@users.noreply.github.com>
2024-09-06 22:12:37 +0300

    Update App.jsx v3

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,21 @@ function App() {
     <div className="app-container">
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
-
         <div className="container text-center content">
-          <div className="row justify-content-center landing_content">
-            <div className="col-12">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-md-10 col-sm-12">
               <h1 className="display-4">Welcome To Paradise Nursery</h1>
-              <div className="divider my-3"></div>
               <p className="lead">Where Green Meets Serenity</p>
-            </div>
-            <div className="col-12 mt-4">
               <button
-                className="btn btn-success btn-lg get-started-button"
+                className="btn btn-success btn-lg mt-3"
                 onClick={handleGetStartedClick}
               >
                 Get Started
               </button>
             </div>
           </div>
-
           <div className="row mt-5">
-            <div className="col-12 aboutus_container">
+            <div className="col-12">
               <AboutUs />
             </div>
           </div>
