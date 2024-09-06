d5b17e9f991207f7d6c696890db1f3f76ec67eac
alimuthana222 <153876752+alimuthana222@users.noreply.github.com>
2024-09-06 22:07:44 +0300

    Update App.jsx v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-
 import React, { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
@@ -16,22 +14,32 @@ function App() {
     <div className="app-container">
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
-        <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-          </div>
+
+        <div className="container text-center content">
+          <div className="row justify-content-center landing_content">
+            <div className="col-12">
+              <h1 className="display-4">Welcome To Paradise Nursery</h1>
+              <div className="divider my-3"></div>
+              <p className="lead">Where Green Meets Serenity</p>
+            </div>
+            <div className="col-12 mt-4">
+              <button
+                className="btn btn-success btn-lg get-started-button"
+                onClick={handleGetStartedClick}
+              >
+                Get Started
+              </button>
+            </div>
           </div>
 
+          <div className="row mt-5">
+            <div className="col-12 aboutus_container">
+              <AboutUs />
+            </div>
+          </div>
+        </div>
       </div>
+
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
         <ProductList />
       </div>
